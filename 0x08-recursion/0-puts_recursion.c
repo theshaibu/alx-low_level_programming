c0081cbb194293d26a1098e5d0f149b143425e2a
shaibu sulleyman <shaibusulleyman3@gmail.com>
2023-03-08 13:57:08 -0800

    function that prints a string, followed by a new line.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
- * @s: string to print
- *
- * Return: void
+ * _puts_recursion - Print a string
+ * @s: string
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		_putchar('\n');
-		return;
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,3 +13,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
+#endif
