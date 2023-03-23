a22e75b1eae7a1a33a1ddcb7ffc117c0d5691db4
shaibu sulleyman <shaibusulleyman3@gmail.com>
2023-03-23 12:36:00 -0700

    C - Variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNC_POINTERS_H
+#define FUNC_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
+~        
