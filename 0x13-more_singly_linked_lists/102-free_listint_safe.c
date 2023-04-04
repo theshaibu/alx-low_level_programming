4d8d30bfd36b3efa1a29ad481447fa145e93c3b9
shaibu sulleyman <shaibusulleyman3@gmail.com>
2023-04-04 15:09:05 -0700

    advamced tasks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,8 +1,11 @@
-#include "lists.h"
+include "lists.h"
+
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
 
 /**
  * looped_listint_count - Counts the number of unique nodes
- *                        in a looped listint_t linked list.
+ *                      in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
  *
  * Return: If the list is not looped - 0.
@@ -13,11 +16,11 @@ size_t looped_listint_count(listint_t *head)
 	listint_t *tortoise, *hare;
 	size_t nodes = 1;
 
-	if (!head || !head->next)
+	if (head == NULL || head->next == NULL)
 		return (0);
 
 	tortoise = head->next;
-	hare = head->next->next;
+	hare = (head->next)->next;
 
 	while (hare)
 	{
@@ -42,7 +45,7 @@ size_t looped_listint_count(listint_t *head)
 		}
 
 		tortoise = tortoise->next;
-		hare = hare->next ? hare->next->next : NULL;
+		hare = (hare->next)->next;
 	}
 
 	return (0);
@@ -67,13 +70,14 @@ size_t free_listint_safe(listint_t **h)
 
 	if (nodes == 0)
 	{
-		for (; h && *h; nodes++)
+		for (; h != NULL && *h != NULL; nodes++)
 		{
 			tmp = (*h)->next;
 			free(*h);
 			*h = tmp;
 		}
 	}
+
 	else
 	{
 		for (index = 0; index < nodes; index++)
@@ -82,9 +86,11 @@ size_t free_listint_safe(listint_t **h)
 			free(*h);
 			*h = tmp;
 		}
+
 		*h = NULL;
 	}
 
+	h = NULL;
+
 	return (nodes);
 }
-
