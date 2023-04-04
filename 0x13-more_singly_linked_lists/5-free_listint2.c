ddf0b0491472309baa6b491c680cd12c7f047c8a
shaibu sulleyman <shaibusulleyman3@gmail.com>
2023-04-04 13:56:43 -0700

    corrections

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,18 +9,18 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp;
+
 	if (head == NULL)
 		return;
 
-	listint_t *tmp;
-
-	while (*head != NULL)
+	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
 
-	*head = NULL;
+	head = NULL;
 }
 
