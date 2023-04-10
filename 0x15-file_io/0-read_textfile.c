2260c1a64340101cbf88a5bb78ef8755bfe9d9ff
shaibu sulleyman <shaibusulleyman3@gmail.com>
2023-04-10 10:01:18 -0700

    function that reads a text file and prints

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,52 +2,31 @@
 
 /**
  * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: number of letters to read and print.
- *
- * Return: number of letters printed. If it fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nrd, nwr;
-    char *buf;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-    if (filename == NULL)
-        return 0;
+	if (!filename)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
+	fd = open(filename, O_RDONLY);
 
-    buf = malloc(sizeof(char) * (letters + 1));
-    if (buf == NULL)
-    {
-        close(fd);
-        return 0;
-    }
+	if (fd == -1)
+		return (0);
 
-    nrd = read(fd, buf, letters);
-    if (nrd == -1)
-    {
-        free(buf);
-        close(fd);
-        return 0;
-    }
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-    buf[nrd] = '\0';
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-    nwr = write(STDOUT_FILENO, buf, nrd);
-    if (nwr == -1 || nwr != nrd)
-    {
-        free(buf);
-        close(fd);
-        return 0;
-    }
+	close(fd);
 
-    free(buf);
-    close(fd);
+	free(buf);
 
-    return nwr;
+	return (nwr);
 }
-
