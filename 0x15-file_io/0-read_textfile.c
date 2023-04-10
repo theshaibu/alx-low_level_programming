032316f5d481fd1d7e53c2028da9e3a82d136240
shaibu sulleyman <shaibusulleyman3@gmail.com>
2023-04-10 05:13:17 -0700

    function that reads a text file and prints

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: number of letters to read and print.
+ *
+ * Return: number of letters printed. If it fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if (filename == NULL) {
+    int fd;
+    ssize_t nrd, nwr;
+    char *buf;
+
+    if (filename == NULL)
+        return 0;
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return 0;
+
+    buf = malloc(sizeof(char) * (letters + 1));
+    if (buf == NULL)
+    {
+        close(fd);
         return 0;
     }
 
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
+    nrd = read(fd, buf, letters);
+    if (nrd == -1)
+    {
+        free(buf);
+        close(fd);
         return 0;
     }
 
-    char *buffer = (char*)malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL) {
-        fclose(fp);
+    buf[nrd] = '\0';
+
+    nwr = write(STDOUT_FILENO, buf, nrd);
+    if (nwr == -1 || nwr != nrd)
+    {
+        free(buf);
+        close(fd);
         return 0;
     }
 
-    ssize_t bytes_read = fread(buffer, sizeof(char), letters, fp);
-    buffer[bytes_read] = '\0';
+    free(buf);
+    close(fd);
 
-    if (bytes_read <= 0 || fwrite(buffer, sizeof(char), bytes_read, stdout) != bytes_read) {
-        free(buffer);
-        fclose(fp);
-        return 0;
-    }
-
-    free(buffer);
-    fclose(fp);
-    return bytes_read;
+    return nwr;
 }
 
