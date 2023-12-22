b525a459e50c43c164261674d03aa75e9d60b235
shaibu sulleyman <shaibusulleyman3@gmail.com>
2023-12-22 06:32:58 -0800

    hash function implementing the djb2 algorithm.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 
 /**
  * hash_table_create - creates a hash table
@@ -9,23 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
 
-    ht->size = size;
-    ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return (NULL);
-    }
+	ht->size = size;
+	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+		return (NULL);
 
-    return (ht);
+	return (ht);
 }
-
